3f393ccae5791cacdf9d2e7b62bd0d46848d1780
babyboy97 <roy.chap13@gmail.com>
2022-01-23 12:56:22 -0800

    feat: added closing cursor functionality

--- DatabaseManagement.py
+++ DatabaseManagement.py
@@ -16,7 +16,7 @@ class SQL:
                             password=self.password,
                             database=self.database
                         )
-        self.my_cursor = self.my_database.cursor()
+        # self.cursor = self.my_database.cursor()
 
     def query_data(self, query:str):
         """Provides the ability to query data from the database
@@ -31,38 +31,47 @@ class SQL:
         Returns:
             df (pd.DataFrame): Dataframe
         """
+        cursor = self.my_database.cursor()
+
         if not isinstance(query,str): 
             raise Exception("Must pass str for query")
         
         try:
-            self.my_cursor.execute(query)
-            columns = self.my_cursor.description
-            data = [{columns[index][0]:column for index, column in enumerate(value)} for value in self.my_cursor.fetchall()]
+            cursor.execute(query)
+            columns = cursor.description
+            data = [{columns[index][0]:column for index, column in enumerate(value)} for value in cursor.fetchall()]
             df = pd.DataFrame(data)
+            cursor.close()
             return df
         except Exception as e:
+            cursor.close()
             raise e
         
-    def insert_date(self, query:str):
+    def insert_data(self, query:str):
         """Calls the initate function then passes the query to insert data
 
         Args:
             query (str): SQL query function
 
-        Exception: Must pass str type for query variable
+        Exception: Must pass str type for query variable 
         """
         if not isinstance(query,str): 
             raise Exception("Must pass str for query")
-                    
+        
+        cursor = self.my_database.cursor()          
+        
         try:
-            self.my_cursor.execute(query)
+            pass
+            cursor.execute(query)
             self.my_database.commit()
         except Exception as e:
             raise e
+        
+        cursor.close()
 
 
 class Lifts(SQL): 
-    def new_lift(self, lift:int, set_number:int, reps:int, weight:int, lift_date:str):
+    def new_lift(self, user_id:int, lift:int, set_number:int, reps:int, weight:int, lift_date:str):
         """Provide ability to import into the lift database
 
         Args:
@@ -71,27 +80,32 @@ class Lifts(SQL):
             weight (int): Weight lifted for each rep in the set
             lift_date (str): Date performed
 
+        Exception: Must pass int for user_id
         Exception: Must provide integar type for set_number, reps, weight, lift_date
         """
-        
+        if not isinstance(user_id, int): 
+            raise Exception("Must pass int for user_id")
         if not all([isinstance(lift,int),isinstance(set_number,int),isinstance(reps,int),isinstance(weight,int)]):
             raise Exception("Please provide integar type for set_number, reps, weight, lift_date")
 
-        query = f"""INSERT INTO WEIGHTLIFTING.LIFTS (LIFT,SET_NUMBER,REPS,BODY_WEIGHT,LIFT_DATE) 
-                    VALUES ({lift},{set_number},{reps},{weight},'{lift_date}');"""
+        query = f"""INSERT INTO WEIGHTLIFTING.LIFTS (USER_ID,LIFT,SET_NUMBER,REPS,WEIGHT,LIFT_DATE) 
+                    VALUES ({user_id}, {lift},{set_number},{reps},{weight},'{lift_date}');"""
 
         SQL.insert_data(self,query)
 
     
 class Weight(SQL):
-    def new_weight(self, weight:int, weight_date:str):
-        if not isinstance(weight,int): 
-            raise Exception("Must provide integar type for weight")
+    def new_weight(self, user_id:int, weight:float, weight_date:str):
+        try: 
+            weight = round(float(weight), 2)
+        except Exception:
+            raise Exception("Must provide float type for weight")
+
         if not isinstance(weight_date,str):
             raise Exception("Must provide str type for weight_date")
 
-        query = f"""INSERT INTO WEIGHTLIFTING.WEIGHT (WEIGHT, WEIGHT_DATE) 
-                    VALUES ({weight},'{weight_date}');"""
+        query = f"""INSERT INTO WEIGHTLIFTING.BODY_WEIGHT (USER_ID, WEIGHT, WEIGHT_DATE) 
+                    VALUES ({user_id}, {weight},'{weight_date}');"""
         
         SQL.insert_data(self,query)
 
@@ -100,8 +114,12 @@ class User(SQL):
         if not all([isinstance(first_name, str), isinstance(last_name, str), isinstance(email, str), isinstance(user_password, str)]):
             raise Exception("Please pass str for each variable")
 
+        # Dupe Email check
+        if len(W.query_data(f"""SELECT * FROM WEIGHTLIFTING.USERS U WHERE LOWER(U.EMAIL) LIKE LOWER('{email}')""")) > 0:
+            raise Exception("Email address already in use. Please choose another.")        
+
         query = f"""INSERT INTO WEIGHTLIFTING.USERS (FIRST_NAME,LAST_NAME,EMAIL,USER_PASSWORD) 
-                    VALUES ('{first_name}', '{last_name}', '{email}', '{user_password}');"""
+                    VALUES ('{first_name.strip()}', '{last_name.strip()}', '{email}', '{user_password}');"""
 
         SQL.insert_data(self,query)
         
